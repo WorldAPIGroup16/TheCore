d4f5c94da6d64c7dcaa64d1470f4d5fc8dbbc22a
komali2 <rogersjcaleb@gmail.com>
2016-09-10 15:02:01 -0700

    core: basic server setup for dev and production

--- server/server.js
+++ server/server.js
@@ -1,8 +1,51 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
+/* eslint no-console: 0 */
 
-app.use(express.static(__dirname + '/../client'));
-app.listen(port);
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../webpack.config.js');
 
-require('./middleware.js')(app, express);
+const isDeveloping = process.env.NODE_ENV !== 'production';
+const port = isDeveloping ? 3000 : process.env.PORT;
+const app = express();
+
+if (isDeveloping) {
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../index.html')));
+    res.end();
+  });
+} 
+///////////////////
+/*PRODUCTIOn*/
+///////////////
+else {
+  app.use(express.static(__dirname + '../dist'));
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
