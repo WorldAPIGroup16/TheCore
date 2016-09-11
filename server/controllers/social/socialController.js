622cfd5cc77a7112c3b26adfa7f251eb4a6f72c5
komali2 <rogersjcaleb@gmail.com>
2016-09-10 20:16:11 -0700

    dev: socialcontroller skeleton

---
+++ server/controllers/social/socialController.js
@@ -0,0 +1,20 @@
+var api = {};
+
+api.updateScore = function(user){
+  return new Promise((resolve, reject)=>{
+    // market.getScore.then((score)=>{
+    //   //
+    // });
+    //chain off facebook  and twitter and then 
+    //resolve(finalScore);
+  });
+};
+
+api.getScore = function(user){
+  return new Promise((resolve, reject)=>{
+    //db.getSocialScore.then
+    //resolve(gottenScore);
+  });
+};
+
+return api;
