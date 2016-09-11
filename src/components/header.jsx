dd3d0a67348e37356ffcb237690a89bcc0ddabbf
Alex Paczynski <apaczy@gmail.com>
2016-09-11 11:55:50 -0700

    des: added home page core icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ export default class Header extends Component {
                   <button type="button" data-toggle="collapse" data-target="#navbar-collapse" className="navbar-toggle"><span className="sr-only">Toggle navigation</span><span className="icon-bar"></span><span className="icon-bar"></span><span className="icon-bar"></span></button>
                   <h1 className="navbar-logo">
                     <Link to="/">
-                    <img src="http://i.imgur.com/3zTH12v.png" alt="logo"/>
+                    <img className='coreLogo' src="./core-icon3.jpeg" alt="logo"/>
                     <span>The Core</span>
                     </Link></h1>
                 </div>
