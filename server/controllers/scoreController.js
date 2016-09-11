385de2ebbab23ed09966b60ec7a47d54b5c22529
komali2 <rogersjcaleb@gmail.com>
2016-09-10 20:15:13 -0700

    dev: scoreController skeleton

---
+++ server/controllers/scoreController.js
@@ -0,0 +1,20 @@
+var api = {};
+
+api.updateScore = function(user){
+  return new Promise((resolve, reject)=>{
+    // market.getScore.then((score)=>{
+    //   //
+    // });
+    //chain off market and social and then 
+    //resolve(finalScore);
+  });
+};
+
+api.getScore = function(user){
+  return new Promise((resolve, reject)=>{
+    //db.getScore.then
+    //resolve(gottenScore);
+  });
+};
+
+return api;
