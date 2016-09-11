bcced38661e79c61b4a4d5df46bbd43151c2b9aa
komali2 <rogersjcaleb@gmail.com>
2016-09-10 20:49:35 -0700

    core: haven changed to promise

--- server/controllers/util/havenController.js
+++ server/controllers/util/havenController.js
@@ -3,16 +3,19 @@ var request = require('request');
 var api = {};
 
 api.analyseSentiment = function(str, cb){
-  var key = PROCESS.ENV.haven;
-  var options = {
-    method: 'GET',
-    url: `	https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?apikey=${key}&text=${str}`
-  };
+  return new Promise((resolve, reject)=>{
+    var key = PROCESS.ENV.haven;
+    var options = {
+      method: 'GET',
+      url: `	https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?apikey=${key}&text=${str}`
+    };
 
-  request(options, (error, response, body)=>{
-    if(error){
+    request(options, (error, response, body)=>{
+      if(error){
+        reject(error);
+      }
+      resolve(body);
+    });
 
-    }
-    cb(body);
   });
 };
